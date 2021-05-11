08d4cbd880ebb646dbaf584c08b78c1d48973bc1
ANTHONY SCARDAPANE <an.scardapane@gmail.com>
2021-05-10 22:23:35 -0400

    update job, make title in card tilty

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Footer from "./footer/footer";
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <InfoCard title="About Me &#128056;">
-          Hey there, I'm Anthony Scardapane. A software engineer who loves to
-          build things that are useful and make people happy. Currently working
-          at <a href="https://vanguard.com">Vanguard</a> where I use Angular,
-          Node.js, Java, and Python. I've been doing this for
-          <strong> 4 years</strong> and feel extremely lucky to love it so much.
+          Hey there, I'm Anthony Scardapane.{" "}
+          <p>
+            A software engineer who loves to build things that are useful and
+            make people happy.
+          </p>{" "}
+          Currently working at{" "}
+          <a href="https://www.cisco.com/c/m/en_us/customer-experience/cx-cloud.html">
+            Cisco!
+          </a>{" "}
         </InfoCard>
         <div className="second-card"></div>
         <InfoCard title="Other stuff &#129510;">
-          Writing at{" "}
-          <a href="https://eventuallyinteresting.net">Eventually Interesting</a>
-          <br></br>
-          <br></br>
           Follow me on Twitter{" "}
           <a href="https://twitter.com/a_scardapane">@a_scardapane</a>
           <br></br>
@@ -26,7 +26,7 @@ const App = () => {
           Judge my unfinished projects and OSS contributions at{" "}
           <a href="https://github.com/a-scar">Github</a>
         </InfoCard>
-      </header>
+      </div>
 
       <Footer>© 2021 Anthony Scardapane. All Rights Reserved.</Footer>
     </div>
